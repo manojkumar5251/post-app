bd775944ca9b0a6b9107c6e85887b6aef0296067
Manoj Kumar <manojkumar5251@gmail.com>
2020-05-29 12:12:35 +0530

    Upoad comments and Api done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ export default class App extends React.PureComponent {
       uploading: false,
       author: "",
       comment: "",
-      comments: { author: "manoj", comment: "manoj", up: 1, down: 2 }
+      comments: {
+        author: "manoj",
+        comment: "manoj",
+        up: 1,
+        down: 2
+      }
     }
   }
 
@@ -23,20 +28,82 @@ export default class App extends React.PureComponent {
       .catch(err => console.log(err))
   }
 
+  uploadComments = () => {
+    let { author, comment } = this.state
+    if (author === "" || comment === "") {
+      return
+    }
+
+    let data = { author, comment, up: 0, down: 0 }
+    this.setState({ uploading: true })
+    Axios.post("/comments.json", data)
+      .then(res => {
+        this.setState(state => {
+          return {
+            uploading: false,
+            author: "",
+            comment: "",
+            comments: {
+              ...state.comments,
+              [res.data.name]: data
+            }
+          }
+        })
+        console.log(this.state.comments)
+      })
+      .catch(err => {
+        this.setState({
+          uploading: false,
+          author: "",
+          comment: ""
+        })
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div className="container d-flex flex-column w-50 mt-3 p-5 rounded">
-        {Object.values(this.state.comments).map((comments, i) => {
-          return (
-            <CommentsList
-              index={i + 1}
-              author={comments.author}
-              comment={comments.comment}
-              up={comments.up}
-              down={comments.down}
-            />
-          )
-        })}
+        <input
+          value={this.state.author}
+          onChange={e => this.setState({ author: e.target.value })}
+          className="form-control font-weight-bold mb-3"
+          placeholder="Author"
+        />
+        <textarea
+          value={this.state.comment}
+          onChange={e => this.setState({ comment: e.target.value })}
+          className="form-control font-weight-bold"
+          rows="3"
+          placeholder="Type a Comment"
+        ></textarea>
+        <button
+          onClick={this.uploadComments}
+          type="button"
+          className="btn btn-danger mt-3 align-self-end font-weight-bold"
+        >
+          {this.state.uploading ? (
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          ) : (
+            "Post Comment"
+          )}
+        </button>
+        <div className="w-100 mt-3 py-3">
+          {Object.values(this.state.comments).map((comments, i) => {
+            return (
+              <CommentsList
+                key={i}
+                index={i + 1}
+                author={comments.author}
+                comment={comments.comment}
+                up={comments.up}
+                down={comments.down}
+              />
+            )
+          })}
+        </div>
       </div>
     )
   }
